beb41ca4a57cf006653b0c9fe4aa0ec1a5333dde
matthewjmckinley <matthewjamesmckinley@gmail.com>
2021-07-13 13:37:46 -0700

    i18n

--- config/module.config.php
+++ config/module.config.php
@@ -2,6 +2,16 @@
 namespace ItemCarouselBlock;
 
 return [
+    'translator' => [
+        'translation_file_patterns' => [
+            [
+                'type' => 'gettext',
+                'base_dir' => OMEKA_PATH . '/modules/ItemCarouselBlock/language',
+                'pattern' => '%s.mo',
+                'text_domain' => null,
+            ],
+        ],
+    ],
     'view_manager' => [
         'template_path_stack' => [
             dirname(__DIR__) . '/view',
