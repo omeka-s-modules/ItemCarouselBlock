a28f672c021271b63bb03aeaf6a653a50625bc85
Jim Safley <jimsafley@gmail.com>
2024-03-14 09:26:45 -0400

    Copy ItemCarouselBlock data for CopyResources

--- Module.php
+++ Module.php
@@ -2,6 +2,7 @@
 namespace ItemCarouselBlock;
 
 use Omeka\Module\AbstractModule;
+use Laminas\EventManager\Event;
 use Zend\EventManager\SharedEventManagerInterface;
 use Zend\Mvc\MvcEvent;
 
@@ -10,5 +11,19 @@ class Module extends AbstractModule
 	public function getConfig()
 	{
 		return include sprintf('%s/config/module.config.php', __DIR__);
+	}
+    public function attachListeners(SharedEventManagerInterface $sharedEventManager)
+    {
+        // Copy ItemCarouselBlock-related data for the CopyResources module.
+        $sharedEventManager->attach(
+            '*',
+            'copy_resources.copy_site',
+            function (Event $event) {
+                $copyResources = $event->getParam('copy_resources');
+                $siteCopy = $event->getParam('resource_copy');
+
+                $copyResources->revertSiteBlockLayouts($siteCopy->id(), 'carousel');
+            }
+        );
 	}
 }
